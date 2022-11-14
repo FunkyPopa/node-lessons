86690ff8a25824d988a86e045f1968d545369b41
FunkyPopa <andriybuno370@gmail.com>
2022-11-15 00:48:35 +0200

    init commit

--- app.js
+++ app.js
@@ -1,32 +1,55 @@
+const express = require('express');
 const fs = require('fs');
 
-fs.readdir('./boys', {withFileTypes: true},(err, files) => {
-    for (const file of files) {
-        if (file.isDirectory()) {
-                console.log(file);
-        } else fs.readFile(`./boys/${file.name}`, (err, data) => {
-          const parsedData = JSON.parse(data);
-           if (parsedData.gender === "female") {
-                fs.rename(`./boys/${file.name}`, `./girls/${file.name}`, (err) => {
-                    console.log(err);
-                });
-             }
-          });
-    }
+const userDB = require('./users/users.json');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.listen(5000, () => {
+    console.log('Server listen 5000');
 });
 
+app.get('/users', (req, res) => {
+    res.json(userDB);
+});
 
-fs.readdir('./girls', {withFileTypes: true},(err, files) => {
-    for (const file of files) {
-        if (file.isDirectory()) {
-            console.log(file);
-        } else fs.readFile(`./girls/${file.name}`, (err, data) => {
-            const parsedData = JSON.parse(data);
-            if (parsedData.gender === "male") {
-                fs.rename(`./girls/${file.name}`, `./boys/${file.name}`, (err) => {
-                    console.log(err);
-                });
+app.get('/users/:userId', (req, res) => {
+
+    const { userId } = req.params;
+    res.json(userDB[userId]);
+});
+
+app.post('/users', (req, res) => {
+
+    const userInfo = req.body;
+    console.log(userInfo);
+
+    res.status(201).json('Created!');
+
+    fs.readdir('./users', (err, files) => {
+        console.log(files);
+            for (const file of files) {
+                console.log(file);
+                fs.readFile(`./users/${file}`, (err, data) => {
+                    console.log(data.);
+                })
             }
-        });
-    }
+        })
+});
+
+app.put('/users', (req, res) => {
+
+
+})
+
+app.patch('/users', (req, res) => {
+
+
+});
+
+app.delete('/users', (req, res) => {
+
 });
