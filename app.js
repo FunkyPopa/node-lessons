b81a7c6175dc0e88d39a7b46322029eaeb19e42d
FunkyPopa <andriybuno370@gmail.com>
2022-12-26 20:55:24 +0200

    Socket

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const http = require('http');
+const socketIO = require('socket.io');
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const swaggerUI = require('swagger-ui-express');
@@ -10,6 +12,7 @@ const { cronRunner } = require("./cron");
 const swaggerJSON = require('./swagger.json');
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -18,6 +21,45 @@ app.use(express.static('static'));
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 
 app.use(fileUpload());
+
+const io = socketIO(server, { cors: 'http://localhost:80'});
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    // console.log(socket.handshake.auth);
+    // console.log(socket.handshake.query);
+
+    socket.on('message:send', (messageData) => {
+        console.log(messageData.text);
+
+        //send one to one
+        socket.emit('message:new', messageData.text);
+        //or maybe something like
+        // socket.to(socket.id).emit('message:new', messageData.text);
+
+        //send event to all except emitter
+        // socket.broadcast.emit('message:new', messageData.text);
+
+        //send event to all clients
+        // io.emit('message:new', messageData.text);
+    });
+
+    socket.on('room:join', (roomInfo) => {
+       socket.join(roomInfo.roomId);
+       // socket.leave(roomInfo.roomId)
+
+        // send to all in room except new member
+       socket.to(roomInfo.roomId).emit('user:room:join', socket.id);
+
+        //send to all in room
+       // io.to(roomInfo.roomId).emit('user:room:join', socket.id);
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Socket ${socket.id} has disconnected`);
+    });
+});
 
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
@@ -31,7 +73,7 @@ app.use((err, req, res, next ) => {
     res.status(err.status || 500).json(err.message);
 });
 
-app.listen(configs.PORT, async () => {
+server.listen(configs.PORT, async () => {
     mongoose.set("strictQuery", false);
     await mongoose.connect(configs.MONGO_URL);
     console.log(`Server listen ${configs.PORT}`);
